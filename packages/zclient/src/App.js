08b906c45be6a56deba5e59dd695dab45492208e
hzamir <hzamir@gmail.com>
2019-09-02 12:30:17 -0400

    kill App.css in favor of styled components

--- packages/zclient/src/App.js
+++ packages/zclient/src/App.js
@@ -1,31 +1,63 @@
 import React from 'react';
+import styled from 'styled-components';
 import logo from './logo.svg';
-import './App.css';
+
 import {guaranteedString} from "./utils/sample-utils";
 
 const a = 'real string';
 const b = undefined;
 
+const AppDiv = styled.div`
+    text-align: center;
+`;
+
+const AppImg = styled.img`
+  @keyframes App-logo-spin {
+    from { transform: rotate(0deg)   }
+    to   { transform: rotate(360deg) }
+ }
+
+ animation: App-logo-spin infinite 20s linear;
+ height: 40vmin;
+ pointer-events: none;
+`;
+
+const AppHeader = styled.header`
+    background-color: #282c34;
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    font-size: calc(10px + 2vmin);
+    color: white;
+`;
+
+const AppLink = styled.a`
+ color: #61dafb;
+`;
+
+
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <AppDiv >
+      <AppHeader>
+        <AppImg src={logo} alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
+        <AppLink
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </AppLink>
         <br/> {guaranteedString(a)} <br/>{guaranteedString(b)}
-      </header>
+      </AppHeader>
 
-    </div>
+    </AppDiv>
   );
 }
 
