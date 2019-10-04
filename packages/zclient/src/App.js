18d2201faa3825d8c120bb1d0effc3b5e0bd362f
hzamir <hzamir@gmail.com>
2019-10-04 14:00:28 -0400

    grid layout, kinda working. Bad colors though. Generated from from Starry Night by Van Gogh, believe it or not.

--- packages/zclient/src/App.js
+++ packages/zclient/src/App.js
@@ -1,42 +1,72 @@
 import React from 'react';
 import styled from 'styled-components';
-import logo from './logo.svg';
 
 import {Ladom} from "./Ladom";
 
-import {guaranteedString} from "./utils/sample-utils";
 
-const a = 'real string';
-const b = undefined;
 
-const AppDiv = styled.div`
-    text-align: center;
+
+const palette = {
+      plum: '#4b54a1',
+      black: '#0c0e0d',
+      blueslate: '#465f73',
+      slate: '#5f5f7b',
+      drab: '#b1c3a9',
+      sky: '#5e86ba',
+      moon: '#b3961e',
+      midnight: '#0b2383',
+
+      gold: 'gold',
+      cornsilk: 'cornsilk',
+      blue: 'blue',
+      forest: 'forestgreen',
+      crimson: 'crimson'
+};
+
+const Layout = styled.div`
+    display:grid;
+    height: calc(100vh);
+    width: calc(100hw);
+    
+    row-gap:4px;
+    column-gap:4px;
+
+    grid-template-columns: 200px minmax(0, 1fr) 400px;
+    grid-template-rows: 30px minmax(0, 1fr) 30px;
+    grid-template-areas: "Navbar Navbar Navbar"
+                         "Left CenterBody Right"
+                         "Footer Footer Footer";    
 `;
 
-const AppImg = styled.img`
-  @keyframes App-logo-spin {
-    from { transform: rotate(0deg)   }
-    to   { transform: rotate(360deg) }
- }
 
- animation: App-logo-spin infinite 20s linear;
- height: 40vmin;
- pointer-events: none;
+const Navbar = styled.section`
+    grid-area: Navbar;
+    background-color: ${palette.midnight};
+    color: ${palette.drab};
+`;
+const Footer = styled.section`
+    grid-row-start:3; 
+    grid-column-start:1; grid-column-end:4;
+    background-color: ${palette.blueslate};
+    color: ${palette.drab};
 `;
 
-const AppHeader = styled.header`
-    background-color: #282c34;
-    min-height: 100vh;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    font-size: calc(10px + 2vmin);
-    color: white;
+const CenterBody = styled.section`
+    display: block;
+    height:100%;
+    grid-area: CenterBody
+    background-color: ${palette.drab};
+    color: ${palette.black};
 `;
-
-const AppLink = styled.a`
- color: #61dafb;
+const Left = styled.section`
+    grid-area: Left
+    background-color: ${palette.cornsilk};
+    color: ${palette.midnight};
+`;
+const Right = styled.section`
+    grid-area: Right
+    background-color: ${palette.cornsilk};
+    color: ${palette.midnight};
 `;
 
 
@@ -46,24 +76,13 @@ const closef=()=>console.warn('closing');
 
 function App() {
   return (
-    <AppDiv >
-      <Ladom content={somejsx} outsideClose={true} close={closef}/>
-      <AppHeader>
-        <AppImg src={logo} alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <AppLink
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </AppLink>
-        <br/> {guaranteedString(a)} <br/>{guaranteedString(b)}
-      </AppHeader>
-
-    </AppDiv>
+      <Layout>
+          <Navbar>There is text here</Navbar>
+          <Left>In left side bar?</Left>
+          <CenterBody>In middle</CenterBody>
+          <Right>In right sidebar?</Right>
+          <Footer>Status stuff is over here</Footer>
+      </Layout>
   );
 }
 
