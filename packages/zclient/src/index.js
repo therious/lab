85d4b056c73403c61eea918b7caea7f049300a7c
hzamir <hzamir@gmail.com>
2019-10-29 21:34:55 -0400

    use createSelector from reselect

--- packages/zclient/src/index.js
+++ packages/zclient/src/index.js
@@ -3,6 +3,7 @@ import { render } from 'react-dom';
 import { createStore, applyMiddleware, combineReducers, bindActionCreators} from 'redux'
 import { Provider, connect } from 'react-redux'
 import { composeWithDevTools } from 'redux-devtools-extension';
+import {createSelector} from 'reselect';
 
 import './index.css';
 import App from './App';
@@ -10,7 +11,6 @@ import {initialState} from "./constants/initial-state";
 import * as actionCreators from './action-creators'
 import * as funcs from './action-funcs';
 import {getMiddleware, init} from "./example-redux-middleware";
-import {crementMiddleWare} from "./example-redux-middleware";
 
 const actionstyle = `
     padding: 2px 8px;
@@ -30,26 +30,28 @@ const rootReducer = combineReducers({myreducer });
 const store = createStore(
     rootReducer,
     composeWithDevTools(
-        applyMiddleware(crementMiddleWare, getMiddleware)
+        applyMiddleware(getMiddleware)
     )
 );
 
 
-let pTrades;
-let pResult;
 
-// todo use reselect library for this
-const tradesToArray = (trades) =>{
-  if(pTrades !== trades) {
-    pTrades = trades;
-    pResult = Object.values(trades);
-  }
-  return pResult;
-};
+// relect code here
+const tradesSelector    = s => s.myreducer.trades;
+const partiesSelector   = s => s.myreducer.parties;
+const quotesSelector    = s => s.myreducer.quotes;
+
+const aTradesSelector  = createSelector(tradesSelector, o=>Object.values(o));
+const aQuotesSelector  = createSelector(quotesSelector, o=>Object.values(o));
+const aPartiesSelector = createSelector(partiesSelector, o=>Object.values(o));
+
 
 const mapStateToProps = state => {
-    const aTrades = tradesToArray(state.myreducer.trades);
-   return {...state.myreducer, aTrades};
+
+    const aTrades  = aTradesSelector(state);
+    const aQuotes  = aQuotesSelector(state);
+    const aParties = aPartiesSelector(state);
+   return {...state.myreducer, aTrades,aQuotes,aParties};
 };
 
 
@@ -61,7 +63,10 @@ const mapDispatchToProps = dispatch => {
 
 const ConnectedApp =  connect(
     mapStateToProps,
-    mapDispatchToProps
+    mapDispatchToProps,
+    null,
+    { forwardRef: true } // must be supplied for react/redux when using Ag-Grid GridOptions.reactNext
+
 )(App);
 
 render(
