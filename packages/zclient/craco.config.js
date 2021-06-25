dc9be326e418ee334fef3bb2bea60c37f74b4438
hzamir <hzamir@gmail.com>
2021-06-25 15:03:48 -0400

    get bundle analyzer working with craco

--- packages/zclient/craco.config.js
+++ packages/zclient/craco.config.js
@@ -1,4 +1,20 @@
+const {BundleAnalyzerPlugin} = require("webpack-bundle-analyzer");
+
+const {env: {REACT_APP_INTERACTIVE_ANALYZE}} = process;
+
+// const analyzerMode = REACT_APP_INTERACTIVE_ANALYZE
+//   ? "server"
+//   : "json"
+
+const webpackPlugins = [
+  new BundleAnalyzerPlugin({ analyzerMode:'server' })
+];
+
+
 module.exports = {
+  webpack: {
+    plugins: webpackPlugins
+  },
   babel: {
     plugins: [
       "babel-plugin-transform-typescript-metadata",
