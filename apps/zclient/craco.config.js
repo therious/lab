ccec784c12ef2b2d5e6a76f625393229e3382e7d
hzamir <hzamir@gmail.com>
2021-07-05 10:17:40 -0400

    yarn stuff that doesn't work

--- apps/zclient/craco.config.js
+++ apps/zclient/craco.config.js
@@ -1,4 +1,13 @@
 const {BundleAnalyzerPlugin} = require("webpack-bundle-analyzer");
+const path = require('path')
+const fs = require('fs')
+const cracoBabelLoader = require('craco-babel-loader')
+
+// Handle relative paths to sibling packages
+const appDirectory = fs.realpathSync(process.cwd())
+const resolvePackage = relativePath => path.resolve(appDirectory, relativePath)
+
+
 
 const {env: {REACT_APP_INTERACTIVE_ANALYZE}} = process;
 
@@ -10,13 +19,26 @@ const webpackPlugins = [
   new BundleAnalyzerPlugin({ analyzerMode:'server' })
 ];
 
+const includes = [
+  'fizbin',
+  'th-utils'
+].map(fn=>resolvePackage(`../../libs/${fn}`));
 
 module.exports = {
   webpack: {
-    plugins: webpackPlugins
+    plugins: webpackPlugins,
+    alias: {
+      "@lib": path.resolve(__dirname, "src/lib/")
+    }
   },
   babel: {
     plugins: [
+      {
+        plugin: cracoBabelLoader,
+        options: {
+          includes,
+        },
+      },
       "babel-plugin-transform-typescript-metadata",
       ["@babel/plugin-proposal-decorators", {legacy: true}],
       ["@babel/plugin-proposal-class-properties", { "loose": true }],
