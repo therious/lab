55fa4671e88c1a95bfeac6bebbf821eb5082f238
hzamir <hzamir@gmail.com>
2021-07-07 15:41:26 -0400

    first cut fsm control class

---
+++ apps/zclient/src/fsm-utils/fsm-control.ts
@@ -0,0 +1,48 @@
+import {createXStateConfiguration} from './convert';
+import {createMachine, interpret,
+  Interpreter, StateMachine,
+  EventObject, EventData, EventType, MachineConfig} from 'xstate';
+
+export interface FsmTransition {
+  from:   string | string[];
+  to:     string;
+  evt?:   string;
+  cond?:  string;
+  after?: number;
+}
+
+export interface FsmConfig {
+  name:         string;
+  states:       string[];
+  transitions:  FsmTransition[];
+  context?:     Record<string,any>;
+  target?:      string;
+}
+
+type FsmContext     = any;
+type FsmStateSchema = any;
+type FsmTypestate   = any;
+type FsmEvent       = EventObject;
+
+
+export type FsmDefinition = StateMachine<FsmContext, FsmStateSchema, FsmEvent>;
+export type FsmInstance = Interpreter<FsmContext, FsmStateSchema, FsmEvent>;
+
+export class FsmControl
+{
+  static define(config:FsmConfig, behavior:any): FsmDefinition
+  {
+     const xstateConfig = createXStateConfiguration(config,behavior) as MachineConfig<any,any,any>;
+     const fsm = createMachine(xstateConfig);
+     return fsm;
+  }
+
+  static instantiate(fsm:FsmDefinition):FsmInstance
+  {
+    const interpreter = interpret(fsm);
+    interpreter.start();
+    return interpreter;
+  }
+
+
+}
