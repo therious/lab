043061a878dd1bfeadefa8d18c712bf2d9f3409c
hzamir <hzamir@gmail.com>
2021-07-07 17:37:49 -0400

    TestClass instantiates and fsm

--- apps/zclient/src/fsm-utils/test-class.ts
+++ apps/zclient/src/fsm-utils/test-class.ts
@@ -1,12 +1,43 @@
 
 import {inject, injectable, container, singleton} from 'tsyringe';
+import { FsmControl, FsmDefinition, FsmInstance, FsmTransition} from './fsm-control';
+
+
+ interface FsmConfig {
+  name:         string;
+  states:       string[];
+  transitions:  FsmTransition[];
+  context?:     Record<string,any>;
+  target?:      string;
+}
+
 
 @singleton()
 export class TestClass
 {
-  constructor(@inject('TestClassParams') readonly testClassParams:any)
+  private fsmdef:FsmDefinition;
+  private fsminst:FsmInstance;
+
+  constructor(
+    @inject('TestClassParams') readonly testClassParams:any,
+    @inject('stateMachines.securityLight') readonly fsmConfig:FsmConfig
+  )
   {
-    console.warn(`constructing TestClass with`, testClassParams);
+    console.warn(`constructing TestClass with`, testClassParams, fsmConfig);
+    this.fsmdef = FsmControl.define(fsmConfig,{});
+
+    const fsmInst = this.fsminst = FsmControl.instantiate(this.fsmdef);
+
+
+    const advance = (token:string)=>{
+      const p = fsmInst.state;
+      fsmInst.send(token);
+      const s = fsmInst.state;
+      console.warn(`!!! pstate = ${p.value} state=${s.value}`, s.context);
+    };
+
+    ['motion','motion'].forEach(advance);
+
   }
 
 }
