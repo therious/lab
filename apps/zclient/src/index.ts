1f8ab439e6de06c6b34225a6cfe8198ce77b5be1
hzamir <hzamir@gmail.com>
2021-07-07 20:06:11 -0400

    fix console statements

--- apps/zclient/src/index.ts
+++ apps/zclient/src/index.ts
@@ -1,19 +1,17 @@
 import "reflect-metadata";
 import {Config, Inflate} from "@therious/boot";
-import {components} from '@therious/components';
 
 // get the relevant classes registered for injection with an import
 import './fsm-utils/test-class';
 import {connectApp} from "./connect-app";
 
-console.log(`components=${components()}`);
-
 (async ()=>{
     try {
         const config = await Config.fetch('/config/hello.yaml');
-        console.warn(`config loaded`);
+        console.warn(`config loaded`,config);
         const inflate = new Inflate(config);
         const extendedConfig = inflate.intializeSequence('bootSequence');
+        console.warn(`extendedConfig `,extendedConfig);
 
         connectApp();
     } catch(e) {
