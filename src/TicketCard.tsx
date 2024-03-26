8063a543930a93c4daf47207a6528735eb15f477
Haim Zamir <hzamir@gmail.com>
2024-03-26 10:17:05 -0400

    add ticket hover wiggle

--- src/TicketCard.tsx
+++ src/TicketCard.tsx
@@ -15,6 +15,29 @@ const TicketDiv = styled.div<{$url:string}>`
   background-origin: border-box;
   background-position: center;
   position: relative;
+
+  &:hover {
+    /* Start the shake animation and make the animation last for 0.5 seconds */
+    animation: shake 0.5s;
+
+    /* When the animation is finished, start again */
+    animation-iteration-count: infinite;
+  }
+
+  @keyframes shake {
+    0%   { transform: translate( 1px,  1px) rotate( 0deg); }
+    10%  { transform: translate(-1px, -2px) rotate(-1deg); }
+    20%  { transform: translate(-3px,  0px) rotate( 1deg); }
+    30%  { transform: translate( 3px,  2px) rotate( 0deg); }
+    40%  { transform: translate( 1px, -1px) rotate( 1deg); }
+    50%  { transform: translate(-1px,  2px) rotate(-1deg); }
+    60%  { transform: translate(-3px,  1px) rotate( 0deg); }
+    70%  { transform: translate( 3px,  1px) rotate(-1deg); }
+    80%  { transform: translate(-1px, -1px) rotate( 1deg); }
+    90%  { transform: translate( 1px,  2px) rotate( 0deg); }
+    100% { transform: translate( 1px, -2px) rotate(-1deg); }
+  }
+  
 `;
 
 // ticket svgs from https://www.svgrepo.com/vectors/ticket/3
