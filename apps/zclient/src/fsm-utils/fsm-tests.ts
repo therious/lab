2d1ca633fd5f12bb9c6f99669e4435d3005838f5
Haim Zamir <hzamir@gmail.com>
2024-03-29 09:39:29 -0400

    zclient gets libs/boot from workspace

--- apps/zclient/src/fsm-utils/fsm-tests.ts
+++ apps/zclient/src/fsm-utils/fsm-tests.ts
@@ -1,5 +1,5 @@
 
-import {inject, container, singleton} from 'tsyringe';
+import {inject, container, singleton} from '@therious/boot';
 import { FsmTest } from './fsm-test';
 
 // todo come up with a way to instantiate multiples directly from configuration without need for subclassing
