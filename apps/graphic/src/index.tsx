048499b67241360a2486db9af3f810be1bdeda99
Haim Zamir <hzamir@gmail.com>
2024-03-29 11:07:42 -0400

    "graphic" app first checkin (broken)

---
+++ apps/graphic/src/index.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import {createRoot} from 'react-dom/client';
+import './index.css';
+import App from './App';
+import {connectRootComponent} from './actions-integration';
+import "reflect-metadata";
+import {Config, Inflate} from "@therious/boot";
+import './fsm/some-tests';
+
+(async ()=>{
+  try {
+    const config = await Config.fetch('/config/example.yaml');
+    console.warn(`config loaded`,config);
+    const inflate = new Inflate(config);
+    const extendedConfig = inflate.intializeSequence('bootSequence');
+    console.warn(`extendedConfig `,extendedConfig);
+
+
+    const RootComponent = connectRootComponent(App) as unknown as React.FunctionComponent;
+    const root = createRoot(document.getElementById('root')!); // createRoot(container!) if you use TypeScript
+    root.render(<RootComponent/>);
+  } catch(e) {
+    console.error(e);
+  }
+})();
