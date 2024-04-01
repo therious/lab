2ba613b1611e5b4e6f794c1b195ba63598f28073
Haim Zamir <hzamir@gmail.com>
2024-04-01 15:22:24 -0400

    ticket inappropriately hosting fsm stuff, that needs to move out of ticket into graphic app?

--- apps/ticket/src/index.tsx
+++ apps/ticket/src/index.tsx
@@ -4,9 +4,19 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {connectRootComponent} from './actions-integration';
+import "reflect-metadata";
+import {Config, Inflate} from "@therious/boot";
+import './fsm/some-tests';
 
 (async ()=>{
   try {
+    const config = await Config.fetch('/config/example.yaml');
+    console.warn(`config loaded`,config);
+    const inflate = new Inflate(config);
+    const extendedConfig = inflate.intializeSequence('bootSequence');
+    console.warn(`extendedConfig `,extendedConfig);
+
+
     const RootComponent = connectRootComponent(App) as unknown as React.FunctionComponent;
     const root = createRoot(document.getElementById('root')!); // createRoot(container!) if you use TypeScript
     root.render(<RootComponent/>);
