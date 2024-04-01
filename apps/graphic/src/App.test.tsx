493b168f77f14cb52fc87e6d0533fa8433988326
Haim Zamir <hzamir@gmail.com>
2024-04-01 15:20:56 -0400

    graphic checkpoint react-query, render fixed dot file

--- apps/graphic/src/App.test.tsx
+++ apps/graphic/src/App.test.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import GraphicApp from './GraphicApp';
 
 test('renders learn react link', () => {
-  render(<App />);
+  render(<GraphicApp />);
 });
