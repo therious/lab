deb9f86d0a22b37fa00b7f2930b734fbafab8d06
Haim Zamir <hzamir@gmail.com>
2024-05-01 02:23:40 -0400

    lectro delete some svgs

--- apps/lectro/src/App.tsx
+++ apps/lectro/src/App.tsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/electron-vite.animate.svg'
 import './App.css'
 
 function App() {
@@ -8,15 +6,8 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://electron-vite.github.io" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+
+      <h1>Vite + React + Electron</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
