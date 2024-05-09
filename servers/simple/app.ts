4c436cc2c23cd3d70890b1a3a592b2fb7fe5e2c0
Haim Zamir <hzamir@gmail.com>
2024-05-08 20:30:40 -0400

    add a simple node server

---
+++ servers/simple/app.ts
@@ -0,0 +1,24 @@
+import fastify from 'fastify';
+
+const app = async () => {
+  const app = fastify();
+
+  app.get('/', (req, reply) => {
+    reply.send('change me to see updates, fastify!');
+  });
+
+  app.get('/ping', (req, reply) => {
+    reply.send({ msg: 'pong' });
+  });
+
+  app.get('/pong', (req, reply) => {
+    reply.send({ msg: 'ping' });
+  });
+
+  if (import.meta.env.PROD)
+    app.listen(3000);
+
+  return app;
+};
+
+export const simpleFastifyServer = app();
