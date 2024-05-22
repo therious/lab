73ef14e90d7b166f1b9d7fc92d9c93960b742490
Haim Zamir <hzamir@gmail.com>
2024-05-22 12:01:06 -0400

    zclient: reenable grid

--- apps/zclient/src/example-redux-middleware.js
+++ apps/zclient/src/example-redux-middleware.js
@@ -1,5 +1,5 @@
-// import axios from 'axios';
-// import {reqIdGenerate} from '@therious/utils';
+import axios from 'axios';
+import {reqIdGenerate} from '@therious/utils';
 
 const middlestyle = `
     padding: 2px 8px;
@@ -49,36 +49,36 @@ export const getMiddleware = store => next => action => {
 
     const aType = action.type || '';
 
-    // const startsWithOms = aType.startsWith('oms');
-    // const isResponse = startsWithOms && aType.endsWith('Response');
-    //
-    // reqIdGenerate();
-    //
-    //
-    // if(startsWithOms && !isResponse)
-    // {
-    //     const rAction = aType+'Response';
-    //     const responsef = (response)=>_actions[rAction](response);
-    //     const catchf = error=>console.error(error);
-    //
-    //     const method = action.post? 'post': 'get';
-    //     const url = createUrl(action,method);
-    //
-    //     console.log(`%c ${aType} sends ${url}`, middlestyle);
-    //
-    //     switch(method)
-    //     {
-    //         case 'post':
-    //             axios.post(url, {...action.body}).then(responsef).catch(catchf);
-    //             break;
-    //         case 'get':
-    //         default:
-    //             if(action.params)
-    //                 axios.get(url, {params:action.params} ).then(responsef).catch(catchf);
-    //             else
-    //                 axios.get(url).then(responsef).catch(catchf);
-    //     }
-    // }
+    const startsWithOms = aType.startsWith('oms');
+    const isResponse = startsWithOms && aType.endsWith('Response');
+
+    reqIdGenerate();
+
+
+    if(startsWithOms && !isResponse)
+    {
+        const rAction = aType+'Response';
+        const responsef = (response)=>_actions[rAction](response);
+        const catchf = error=>console.error(error);
+
+        const method = action.post? 'post': 'get';
+        const url = createUrl(action,method);
+
+        console.log(`%c ${aType} sends ${url}`, middlestyle);
+
+        switch(method)
+        {
+            case 'post':
+                axios.post(url, {...action.body}).then(responsef).catch(catchf);
+                break;
+            case 'get':
+            default:
+                if(action.params)
+                    axios.get(url, {params:action.params} ).then(responsef).catch(catchf);
+                else
+                    axios.get(url).then(responsef).catch(catchf);
+        }
+    }
 
     next(action);
 };
