7b391b034d2607eb7c47b8c1c5a5a9781093d2f3
Haim Zamir <hzamir@gmail.com>
2024-05-24 14:34:31 -0400

    graphic - update

--- apps/graphic/src/-graphic.tsx
+++ apps/graphic/src/-graphic.tsx
@@ -5,9 +5,8 @@ import './index.css';
 import GraphicApp from './GraphicApp';
 import {connectRootComponent} from './actions-integration';
 import {Config, Inflate} from "@therious/boot";
-import {hasAll, hasAny, sleep} from '@therious/utils';
-import * as Proxyable from '@therious/utils';
 import {bootmeta} from '@therious/ddd';
+import { logThrottle } from '@therious/utils'; // imported for side effect tests for now
 
 
 import './fsm/some-tests';
@@ -15,65 +14,14 @@ import './fsm/some-tests';
 const cypher = bootmeta();
 console.log(cypher);
 
-class ExampleProxyable implements Proxyable.NamedInstance
-{
-  getSpec(): ReplicatorSpec { return `ExampleProxyable`; }
-  public static proxyHandler: Proxyable.Handler<ExampleProxyable> = new Proxyable.Handler<ExampleProxyable>(ExampleProxyable, 400);
-  protected isPrimary:boolean;
-
-  constructor(isPrimary:boolean) {
-    this.isPrimary = isPrimary;
-    const proxy = Proxyable.register<ExampleProxyable>(this, ExampleProxyable.proxyHandler, isPrimary);
-    if(proxy)
-      return proxy;
-  }
-
-  async Hello()
-  {
-    console.info('>>>Hello');
-    return `>>>hi isPrimary = ${this.isPrimary}`;
-  }
-
-}
-
-import {
-  Replicator,
-  ReplicatorHub,
-  ReplicatorSpoke,
-  logThrottle,
-  ReplicatorSpec,
-} from '@therious/utils'; // imported for side effect tests for now
 
 (async ()=>{
   try {
-    const config = await Config.fetch('/config/example.yaml');
+    const config = await Config.fetch('/config/-graphic.yaml');
     console.warn(`config loaded`,config);
     const inflate = new Inflate(config);
     const extendedConfig = inflate.intializeSequence('bootSequence');
     console.warn(`extendedConfig `,extendedConfig);
-
-
-
-    if(hasAll(config.queryParams, ['main']))
-    {
-      new ReplicatorHub();
-    }
-    if(hasAny(config.queryParams, ['main', 'side']))
-    {
-      const spoke = new ReplicatorSpoke();
-      await spoke.synced;
-
-    }
-
-    const proxyable = Replicator.Spoke? new ExampleProxyable(!!Replicator.Hub): null;
-
-    await sleep(100);
-
-    if(proxyable) {
-      console.warn(`>>>invoking proxyable`);
-      const result = await proxyable.Hello();
-      console.warn(`>>>invoked proxyable, received result`,result);
-    }
 
     const RootComponent = connectRootComponent(GraphicApp) as unknown as React.FunctionComponent;
     const root = createRoot(document.getElementById('root')!); // createRoot(container!) if you use TypeScript
@@ -83,4 +31,4 @@ import {
   }
 })();
 
-const report = logThrottle(10_000, (msg:string, ...rest:unknown[])=>console.log(msg, ...rest));
+// const report = logThrottle(10_000, (msg:string, ...rest:unknown[])=>console.log(msg, ...rest));
