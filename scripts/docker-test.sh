197a9ba838c2b6bd391f685108e4c4992a433240
Haim Zamir <hzamir@gmail.com>
2024-06-20 18:51:09 -0400

    docker script update

--- scripts/docker-test.sh
+++ scripts/docker-test.sh
@@ -2,12 +2,14 @@
 
 NAME="${1:-suite}" # short name for the docker file, located at docker/<NAME>.dockerfile
 PORT="${2:-3000}"  # port to publish on os (internal 3000 assumed) defaults to 3000
+IPORT="${3:-3000}"  # port to publish on os (internal 3000 assumed) defaults to 3000
+
 DOCKERFILE="docker/${NAME}.dockerfile"
 IMAGE="therious-${NAME}"
 CONTAINER="therious-${NAME}-${PORT}"
 
 if ! [ -f "${DOCKERFILE}" ]; then
-  echo "[$1] File does not exist."
+  echo "File '${DOCKERFILE}' does not exist."
   exit 1
 fi
 
@@ -19,7 +21,7 @@ pnpm f ticket build
 docker kill "${CONTAINER}" || true
 
 # run a docker build
-docker build --nocache --progress=plain -f "${DOCKERFILE}" -t "${IMAGE}" .  2>&1 | tee "${DOCKERFILE}.log"
+docker build --no-cache --progress=plain -f "${DOCKERFILE}" -t "${IMAGE}" .  2>&1 | tee "${DOCKERFILE}.log"
 
 # run the container
-nohup docker run --pull=never -p "${PORT}:3000" --name="${CONTAINER}" "${IMAGE}" &
+nohup docker run --pull=never -p "${PORT}:${IPORT}" --name="${CONTAINER}" "${IMAGE}" &
