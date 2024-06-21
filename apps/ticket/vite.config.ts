3736c7584de87b0c9cb877de9b14a60ca1239888
Haim Zamir <hzamir@gmail.com>
2024-06-20 22:12:11 -0400

    vite-bundle-analyzer

--- apps/ticket/vite.config.ts
+++ apps/ticket/vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { analyzer } from 'vite-bundle-analyzer'
 
 // https://vitejs.dev/config/
 
 export default defineConfig({
-  plugins: [react({tsDecorators:true})],
-})
+  plugins: [react({tsDecorators:true}), analyzer()],
+});
