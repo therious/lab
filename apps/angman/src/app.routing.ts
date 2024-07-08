561c4f0f634a0ea640e17bd507d604db750752ea
Haim Zamir <hzamir@gmail.com>
2024-07-08 16:56:36 -0400

    first angular commit

---
+++ apps/angman/src/app.routing.ts
@@ -0,0 +1,17 @@
+import { Route, provideRouter } from '@angular/router';
+
+const routes: Route[] = [
+  {
+    path: '',
+    loadComponent: () => import('./home.component').then(c => c.HomeComponent)
+  },
+  {
+    path: 'about-us',
+    loadComponent: (): any => import('./about.component').then (c => c.AboutComponent)
+  }
+];
+
+
+export const appRouting = [
+  provideRouter(routes)
+]
