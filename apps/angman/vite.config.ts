30df558b5a0760ce3666c397a9dba18ea837eb58
Haim Zamir <hzamir@gmail.com>
2024-07-08 16:56:36 -0400

    first angular commit

---
+++ apps/angman/vite.config.ts
@@ -0,0 +1,27 @@
+import { splitVendorChunkPlugin, defineConfig } from 'vite';
+import { dependencies } from './package.json';
+
+
+function renderChunks(deps:Record<string, string>) {
+  const chunks:Record<string,string[]> = {};
+  Object.keys(deps).forEach((key) => {
+    // if ([].includes(key)) return;
+    chunks[key] = [key];
+  });
+  return chunks;
+}
+
+export default defineConfig({
+  plugins: [splitVendorChunkPlugin()],
+  build: {
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: [],
+          ...renderChunks(dependencies),
+        },
+      },
+    },
+  },
+})
