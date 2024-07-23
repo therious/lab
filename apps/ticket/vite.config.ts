95d55919ab26166843147f66f1f88ceb7774ef4b
Haim Zamir <hzamir@gmail.com>
2024-07-23 01:07:43 -0400

    disable bundle analyzer

--- apps/ticket/vite.config.ts
+++ apps/ticket/vite.config.ts
@@ -5,5 +5,5 @@ import { analyzer } from 'vite-bundle-analyzer'
 // https://vitejs.dev/config/
 
 export default defineConfig({
-  plugins: [react({tsDecorators:true}), analyzer()],
+  plugins: [react({tsDecorators:true})/*, analyzer()*/],
 });
