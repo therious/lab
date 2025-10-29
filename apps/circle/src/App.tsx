569770ad8873c34b02f68f192acbb2b4ad328ea2
Haim Zamir <hzamir@gmail.com>
2025-10-28 21:03:59 -0400

    circle of fifths code

--- apps/circle/src/App.tsx
+++ apps/circle/src/App.tsx
@@ -1,22 +1,8 @@
-import { useState } from 'react'
+import Fifths from './Fifths'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <h1>Circle</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-    </>
-  )
+  return <Fifths />
 }
 
 export default App
