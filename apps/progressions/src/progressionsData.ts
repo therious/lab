95cce29dbe82f702d6027d3b9a413969d0ef7d57
Haim Zamir <hzamir@gmail.com>
2025-10-28 22:20:16 -0400

    progressions

---
+++ apps/progressions/src/progressionsData.ts
@@ -0,0 +1,95 @@
+import { ChordProgression } from './types';
+
+export const progressionsData: ChordProgression[] = [
+  {
+    name: "I-V-vi-IV",
+    progression: ['I', 'V', 'vi', 'IV'],
+    songs: ["Let It Be (The Beatles)", "Don't Stop Believin' (Journey)", "With or Without You (U2)"],
+    key: "C"
+  },
+  {
+    name: "vi-IV-I-V",
+    progression: ['vi', 'IV', 'I', 'V'],
+    songs: ["Complicated (Avril Lavigne)", "Paparazzi (Lady Gaga)", "Californication (Red Hot Chili Peppers)"],
+    key: "Am"
+  },
+  {
+    name: "I-vi-IV-V",
+    progression: ['I', 'vi', 'IV', 'V'],
+    songs: ["Stand By Me (Ben E. King)", "Where Have All The Flowers Gone (Pete Seeger)"],
+    key: "C"
+  },
+  {
+    name: "vi-I-IV-V",
+    progression: ['vi', 'I', 'IV', 'V'],
+    songs: ["Someone Like You (Adele)", "Rolling in the Deep (Adele)"],
+    key: "Am"
+  },
+  {
+    name: "I-V-IV-V",
+    progression: ['I', 'V', 'IV', 'V'],
+    songs: ["Livin' On A Prayer (Bon Jovi)", "She Loves You (The Beatles)"],
+    key: "C"
+  },
+  {
+    name: "I-IV-vi-V",
+    progression: ['I', 'IV', 'vi', 'V'],
+    songs: ["Hey Jude (The Beatles)", "All of Me (John Legend)"],
+    key: "C"
+  },
+  {
+    name: "I-bVII-IV",
+    progression: ['I', 'bVII', 'IV'],
+    songs: ["Sweet Child O' Mine (Guns N' Roses)", "Sweet Home Alabama (Lynyrd Skynyrd)"],
+    key: "C"
+  },
+  {
+    name: "IV-I-V-vi",
+    progression: ['IV', 'I', 'V', 'vi'],
+    songs: ["Perfect (Ed Sheeran)", "Closer (The Chainsmokers)"],
+    key: "C"
+  },
+  {
+    name: "I-ii-vi-V",
+    progression: ['I', 'ii', 'vi', 'V'],
+    songs: ["Photograph (Ed Sheeran)", "Sugar (Maroon 5)"],
+    key: "C"
+  },
+  {
+    name: "I-bVI-IV-I",
+    progression: ['I', 'bVI', 'IV', 'I'],
+    songs: ["Creep (Radiohead)", "No Surprises (Radiohead)"],
+    key: "C"
+  },
+  {
+    name: "vi-IV-I-V-vi-IV-I-V",
+    progression: ['vi', 'IV', 'I', 'V', 'vi', 'IV', 'I', 'V'],
+    songs: ["Don't Look Back in Anger (Oasis)", "Wonderwall (Oasis)"],
+    key: "Am"
+  },
+  {
+    name: "I-V-I-V",
+    progression: ['I', 'V', 'I', 'V'],
+    songs: ["Blitzkrieg Bop (Ramones)", "Take on Me (A-ha)"],
+    key: "C"
+  },
+  {
+    name: "i-bVII-bVI-bVII",
+    progression: ['i', 'bVII', 'bVI', 'bVII'],
+    songs: ["White Wedding (Billy Idol)", "Running Up That Hill (Kate Bush)"],
+    key: "Am"
+  },
+  {
+    name: "I-IV-V-IV",
+    progression: ['I', 'IV', 'V', 'IV'],
+    songs: ["Sweet Caroline (Neil Diamond)", "La Bamba (Ritchie Valens)"],
+    key: "C"
+  },
+  {
+    name: "vi-ii-V-I",
+    progression: ['vi', 'ii', 'V', 'I'],
+    songs: ["Autumn Leaves", "Girl from Ipanema"],
+    key: "Am"
+  }
+];
+
---
+++ apps/progressions/src/types.ts
@@ -0,0 +1,9 @@
+export type Chord = string; // e.g., 'I', 'i', 'ii', 'IV', 'bVI', 'bVII', 'VII7', 'VIImaj7'
+
+export interface ChordProgression {
+  name: string;
+  progression: Chord[];
+  songs: string[];
+  key: string; // e.g., 'C', 'D', 'Am', etc.
+}
+
