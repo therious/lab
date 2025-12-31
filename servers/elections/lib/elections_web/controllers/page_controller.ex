6122012e386c82b8cb369359a111951e72b35cd4
Haim Zamir <hzamir@gmail.com>
2025-12-31 00:47:44 -0500

    elect: fix token controller compilation error and update page controller to serve React app

--- servers/elections/lib/elections_web/controllers/page_controller.ex
+++ servers/elections/lib/elections_web/controllers/page_controller.ex
@@ -2,6 +2,16 @@ defmodule ElectionsWeb.PageController do
   use ElectionsWeb, :controller
 
   def home(conn, _params) do
-    render(conn, :home)
+    # Serve the React app's index.html
+    index_path = Path.join([:code.priv_dir(:elections), "static", "index.html"])
+    
+    if File.exists?(index_path) do
+      conn
+      |> put_resp_content_type("text/html")
+      |> send_file(200, index_path)
+    else
+      # Fallback if index.html doesn't exist yet
+      render(conn, :home, layout: false)
+    end
   end
 end
